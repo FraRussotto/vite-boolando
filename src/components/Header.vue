8ad61cbcb3f1148097a25808ec30365769293280
FraRussotto <russottofrancesco1@gmail.com>
2023-10-10 16:04:55 +0200

    [bug] Rilevamento color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,35 @@
 </script>
 
 <template>
-  <header>Header</header>
-
+  <header>
+    <div class="nav">
+      <ul>
+        <li>
+          <a href="#">Uomo</a>
+        </li>
+        <li>
+          <a href="#">Donna</a>
+        </li>
+        <li>
+          <a href="#">Bambino</a>
+        </li>
+      </ul>
+    </div>
+    <div class="logo">
+      <img src="/boolean-logo.png" alt="">
+    </div>
+  </header>
 </template>
 
-<style>
+<style lang="scss">
+
+header{
+  height: 80px;
+  background-color: $primary-color;
+}
+
+img{
+  width: 200px;
+}
 
 </style>
