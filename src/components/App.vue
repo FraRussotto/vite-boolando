35c189c6a6bf723ee7b3330b85c5f6a6b63f1364
FraRussotto <russottofrancesco1@gmail.com>
2023-10-10 15:48:46 +0200

    [edit] struttura

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,15 +1,27 @@
 <script>
+
+  import Header from './Header.vue';
+  import Main from './Main.vue';
+  import Footer from './Footer.vue';
+
   export default{
-    name: 'App'
+    name: 'App',
+    components: {
+      Header,
+      Main,
+      Footer
+    }
   }
 </script>
 
 <template>
-  <div>
-    <h1>Vue Boolando</h1>
-  </div>
-
+  <Header />
+  <Main />
+  <Footer />
 </template>
 
-<style>
+<style lang="scss">
+
+@use '../scss/main.scss';
+
 </style>
